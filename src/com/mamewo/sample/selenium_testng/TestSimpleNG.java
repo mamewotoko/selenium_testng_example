695422ca981443cf47026c83b0d3c98d0b90d6fc
Takashi Masuyama <mamewotoko@gmail.com>
2015-03-16 08:57:33 +0900

    add simple testng tset

---
+++ src/com/mamewo/sample/selenium_testng/TestSimpleNG.java
@@ -0,0 +1,23 @@
+package com.mamewo.sample.selenium_testng;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+public class TestSimpleNG {
+	@BeforeTest
+	public void setUp(){
+		System.out.println("before test");
+	}
+
+	@AfterTest
+	public void tearDown(){
+		System.out.println("after test");
+	}
+
+	@Test
+	public void testSimple(){
+		System.out.println("test!");
+	}
+}
+
